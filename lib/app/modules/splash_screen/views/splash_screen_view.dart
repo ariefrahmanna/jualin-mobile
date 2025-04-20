ca49ea5136b6eb2fd5ce00990778393c48bc4a58
Arief Rahman <140642047+ariefrahmanna@users.noreply.github.com>
2025-04-20 14:58:42 +0700

    feat: implement splash screen page

--- lib/app/modules/splash_screen/views/splash_screen_view.dart
+++ lib/app/modules/splash_screen/views/splash_screen_view.dart
@@ -1,22 +1,35 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:jualin/app/routes/app_pages.dart';
+import 'package:jualin/app/themes/colors.dart';
 
 import '../controllers/splash_screen_controller.dart';
 
 class SplashScreenView extends GetView<SplashScreenController> {
   const SplashScreenView({super.key});
+
   @override
   Widget build(BuildContext context) {
+    // TODO: Implement authentication
+    Future.delayed(const Duration(seconds: 5)).then((value) {
+      Get.offAllNamed(Routes.LOGIN);
+    });
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('SplashScreenView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'SplashScreenView is working',
-          style: TextStyle(fontSize: 20),
+      backgroundColor: neutral10,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/logo-2x.png',
+              scale: 2,
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+          ],
         ),
       ),
     );
