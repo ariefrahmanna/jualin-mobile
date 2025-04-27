de1c144d6178d7af1cd2efea5efa9e145da5c6cf
oliverleon <phyromontega@gmail.com>
2025-04-27 16:32:46 +0700

    fix: update color hex in colors.dart

--- lib/app/themes/colors.dart
+++ lib/app/themes/colors.dart
@@ -5,4 +5,4 @@ const Color primaryColor = Color(0xFF156650);
 const Color neutral10 = Color(0xFFFFFFFF);
 const Color neutral50 = Color(0xFFC2C2C2);
 const Color neutral70 = Color(0xFF757575);
-const Color text = Color(0x00404040);
+const Color text = Color(0xFF404040);
