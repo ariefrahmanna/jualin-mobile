96b3f1c642d09d5ecb9be720a4233177ddc81d3a
alexandersrtn <alexander.dprs@gmail.com>
2025-04-29 19:14:08 +0700

    feat: add new WishlistItem class for item on wishlist

--- lib/app/modules/wishlist/views/wishlist_view.dart
+++ lib/app/modules/wishlist/views/wishlist_view.dart
@@ -59,3 +59,82 @@ class WishlistView extends GetView<WishlistController> {
     );
   }
 }
+
+class WishlistItem extends StatelessWidget {
+  final String title;
+  final String price;
+  final String imageName;
+
+  const WishlistItem({
+    Key? key,
+    required this.title,
+    required this.price,
+    required this.imageName,
+  }) : super(key: key);
+ @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      elevation: 4,
+      child: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Row(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: Image.asset(
+                'assets/$imageName',
+                width: 80,
+                height: 80,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(width: 16),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(title,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      )),
+                  const SizedBox(height: 8),
+                  Text(price,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      )),
+                ],
+              ),
+            ),
+            Column(
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.favorite, color: Colors.red),
+                  onPressed: () {
+                    // Tombol favorite ditekan
+                  },
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // Tombol buy ditekan
+                  },
+                  child: const Text('Buy'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    minimumSize: const Size(80, 36),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    textStyle: const TextStyle(fontSize: 14),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
