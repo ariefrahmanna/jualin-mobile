244e9e10341ab13b47da45d9845a6584decd10b4
alexandersrtn <alexander.dprs@gmail.com>
2025-04-29 19:17:25 +0700

    fix: update color hex

--- lib/app/themes/colors.dart
+++ lib/app/themes/colors.dart
@@ -6,3 +6,6 @@ const Color neutral10 = Color(0xFFFFFFFF);
 const Color neutral50 = Color(0xFFC2C2C2);
 const Color neutral70 = Color(0xFF757575);
 const Color text = Color(0xFF404040);
+const Color selectedItemColor = Color(0xFF4CAF50);
+const Color unselectedItemColor = Color(0xFF9E9E9E);
+const Color favorite = Color(0xFFF44336) ;
