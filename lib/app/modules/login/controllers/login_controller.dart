fde35838a81d1652cfab5e2e13838c5d58680465
oliverleon <phyromontega@gmail.com>
2025-06-02 19:56:48 +0700

    feat: enhance login error handling with improved snackbar notifications

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:jualin/app/routes/app_pages.dart';
+import 'package:jualin/app/themes/colors.dart';
 import 'package:jualin/utils/api_endpoints.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
@@ -26,7 +27,13 @@ class LoginController extends GetxController {
     var headers = {'Content-Type': 'application/json'};
 
     if (email.isEmpty || password.isEmpty) {
-      Get.snackbar('Error', 'Email dan Passsword tidak boleh kosong');
+      Get.snackbar(
+        'Error',
+        'Email dan Passsword tidak boleh kosong',
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: errors,
+        colorText: neutral10,
+      );
       return;
     }
 
@@ -34,32 +41,54 @@ class LoginController extends GetxController {
 
     try {
       var url =
-        Uri.parse(ApiEndpoints.baseUrl + ApiEndpoints.authEndpoints.login);
+          Uri.parse(ApiEndpoints.baseUrl + ApiEndpoints.authEndpoints.login);
 
       Map body = {'username': email, 'password': password};
 
       http.Response response =
-        await http.post(url, body: jsonEncode(body), headers: headers);
+          await http.post(url, body: jsonEncode(body), headers: headers);
       final json = jsonDecode(response.body);
 
       if (response.statusCode == 200) {
-
         if (json['status'] == true) {
           var token = json['token'];
-          
+
           final SharedPreferences? prefs = await _prefs;
           await prefs?.setString('token', token.toString());
-          Get.snackbar('Sukses', json['message']);
+          Get.snackbar(
+            'Success',
+            json['message'],
+            snackPosition: SnackPosition.BOTTOM,
+            backgroundColor: success,
+            colorText: neutral10,
+          );
           Get.offAllNamed(Routes.DASHBOARD);
         } else {
-          Get.snackbar('Gagal', json['message']);
+          Get.snackbar(
+            'error',
+            json['message'],
+            snackPosition: SnackPosition.BOTTOM,
+            backgroundColor: errors,
+            colorText: neutral10,
+          );
         }
-
       } else {
-        Get.snackbar('Gagal', json['message']);
+        Get.snackbar(
+          'error',
+          json['message'],
+          snackPosition: SnackPosition.BOTTOM,
+          backgroundColor: errors,
+          colorText: neutral10,
+        );
       }
-    }catch(error) {
-      Get.snackbar('Login Error', error.toString());
+    } catch (error) {
+      Get.snackbar(
+        'error',
+        error.toString(),
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: errors,
+        colorText: neutral10,
+      );
     } finally {
       isLoading.value = false;
     }
