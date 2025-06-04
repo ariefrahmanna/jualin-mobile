e994a131f60db953b564d7e96db085efc0485235
Arief Rahman <140642047+ariefrahmanna@users.noreply.github.com>
2025-06-04 18:46:16 +0700

    refactor: remove debug print statements

--- lib/app/modules/splash_screen/controllers/splash_screen_controller.dart
+++ lib/app/modules/splash_screen/controllers/splash_screen_controller.dart
@@ -4,19 +4,13 @@ import 'package:jualin/app/routes/app_pages.dart';
 
 class SplashScreenController extends GetxController {
   Future<void> checkToken() async {
-    print("SplashScreen: Mulai cek token");
-
     await Future.delayed(Duration(seconds: 2));
     FlutterSecureStorage secureStorage = FlutterSecureStorage();
     String? token = await secureStorage.read(key: 'token');
 
-    print("SplashScreen: Token = $token");
-
     if (token != null && token.isNotEmpty) {
-      print("SplashScreen: Navigasi ke DASHBOARD");
       Get.offAllNamed(Routes.DASHBOARD);
     } else {
-      print("SplashScreen: Navigasi ke LOGIN");
       Get.offAllNamed(Routes.LOGIN);
     }
   }
