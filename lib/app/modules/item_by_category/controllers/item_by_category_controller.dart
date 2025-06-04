b382e8791670b7b1b38afeade26f5d5f0f94dbd1
Arief Rahman <140642047+ariefrahmanna@users.noreply.github.com>
2025-06-04 16:37:25 +0700

    refactor: streamline fetch items by category

--- lib/app/modules/item_by_category/controllers/item_by_category_controller.dart
+++ lib/app/modules/item_by_category/controllers/item_by_category_controller.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:jualin/app/themes/colors.dart';
 import 'package:jualin/utils/api_endpoints.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -23,31 +24,35 @@ class ItemByCategoryController extends GetxController {
     var secureStorage = FlutterSecureStorage();
     String? token = await secureStorage.read(key: 'token');
     try {
-      final url = Uri.parse(
+      var url = Uri.parse(
         '${ApiEndpoints.baseUrl}${ApiEndpoints.authEndpoints.items}?category=$category',
       );
-      final headers = {
-        'Authorization': 'Bearer $token',
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-      };
-      final response = await http.get(
+
+      var response = await http.get(
         url,
-        headers: headers,
+        headers: {
+          'Authorization': 'Bearer $token',
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
       );
-      print("Token: $token");
-      print("URL: $url");
-      print("Headers: $headers");
+
       if (response.statusCode == 200) {
         final List data = json.decode(response.body)['data'];
         items.value = data;
       } else {
         items.clear();
-        Get.snackbar('Error', 'Gagal memuat data item');
+        Get.snackbar('Error', 'Failed to load items');
       }
     } catch (e) {
       items.clear();
-      Get.snackbar('Error', 'Terjadi kesalahan');
+      Get.snackbar(
+        'Error',
+        e.toString(),
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: errors,
+        colorText: neutral10,
+      );
     } finally {
       isLoading.value = false;
     }
