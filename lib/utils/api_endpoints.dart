88266d2fb1fa5f70ec898639ef241eae4044512b
Arief Rahman <140642047+ariefrahmanna@users.noreply.github.com>
2025-06-04 16:30:03 +0700

    fix: revert base URL to local development server

--- lib/utils/api_endpoints.dart
+++ lib/utils/api_endpoints.dart
@@ -1,5 +1,5 @@
 class ApiEndpoints {
-  static final String baseUrl = 'https://8454-2001-448a-3023-248a-c05e-e841-9646-7817.ngrok-free.app/api/';
+  static final String baseUrl = 'http://10.0.2.2:8000/api/';
   static AuthEndpoints authEndpoints = AuthEndpoints();
 }
 
