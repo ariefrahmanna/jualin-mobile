b40b09387f003356b5c23506c5e7b8a980864bab
oliverleon <phyromontega@gmail.com>
2025-06-05 09:46:02 +0700

    feat: implement search functionality in HomeController

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,10 +1,17 @@
+import 'dart:convert';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get/get.dart';
 import 'package:jualin/app/modules/recently_added/controllers/recently_added_controller.dart';
 import 'package:jualin/app/themes/colors.dart';
+import 'package:http/http.dart' as http;
+import 'package:jualin/utils/api_endpoints.dart';
 
 class HomeController extends GetxController {
   var recentlyAddedItems = [].obs;
+  var searchResults = [].obs;
   var isLoading = false.obs;
+  var isSearching = false.obs;
 
   var recentlyAddedController = Get.put(RecentlyAddedController());
 
@@ -14,7 +21,8 @@ class HomeController extends GetxController {
       await recentlyAddedController.fetchRecentlyAddedItems();
 
       // Ambil hanya 5 item sebagai preview
-      recentlyAddedItems.value = recentlyAddedController.recentlyAddedItems.take(5).toList();
+      recentlyAddedItems.value =
+          recentlyAddedController.recentlyAddedItems.take(5).toList();
     } catch (e) {
       Get.snackbar(
         'Error',
@@ -24,6 +32,42 @@ class HomeController extends GetxController {
       );
     } finally {
       isLoading.value = false;
+    }
+  }
+
+  Future<void> searchItems(String query) async {
+    if (query.isEmpty) {
+      searchResults.clear();
+      return;
+    }
+
+    try {
+      var secureStorage = FlutterSecureStorage();
+      String? token = await secureStorage.read(key: 'token');
+      var url = Uri.parse(
+          '${ApiEndpoints.baseUrl}${ApiEndpoints.authEndpoints.items}?search=$query');
+      var headers = {
+        'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      };
+      var response = await http.get(url, headers: headers);
+
+      isSearching.value = true;
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body)['data'];
+        searchResults.value = data;
+      } else {
+        searchResults.clear();
+        Get.snackbar('Error', 'Search failed',
+            backgroundColor: errors, colorText: neutral10);
+      }
+    } catch (e) {
+      searchResults.clear();
+      Get.snackbar('Error', e.toString(),
+          backgroundColor: errors, colorText: neutral10);
+    } finally {
+      isSearching.value = false;
     }
   }
 
