9d44ab67dca1db4db37e8b2fb7fa27eb86196bcb
Arief Rahman <140642047+ariefrahmanna@users.noreply.github.com>
2025-06-05 20:23:10 +0700

    style: update color scheme for consistency across dashboard and home views

--- lib/app/themes/colors.dart
+++ lib/app/themes/colors.dart
@@ -3,8 +3,16 @@ import 'package:flutter/material.dart';
 const Color primaryColor = Color(0xFF156650);
 
 const Color neutral10 = Color(0xFFFFFFFF);
+const Color neutral20 = Color(0xFFE6E6E6);
+const Color neutral30 = Color(0xFFCCCCCC);
+const Color neutral40 = Color(0xFFB3B3B3);
 const Color neutral50 = Color(0xFFC2C2C2);
+const Color neutral60 = Color(0xFF999999);
 const Color neutral70 = Color(0xFF757575);
+const Color neutral80 = Color(0xFF4D4D4D);
+const Color neutral90 = Color(0xFF1A1A1A);
+const Color neutral100 = Color(0xFF000000);
+
 const Color text = Color(0xFF404040);
 const Color selectedItemColor = Color(0xFF4CAF50);
 const Color unselectedItemColor = Color(0xFF9E9E9E);
