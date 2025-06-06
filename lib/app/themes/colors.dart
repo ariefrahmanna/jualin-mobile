14ef8e0503405df07e63f16d7c503dd09d237354
Arief Rahman <140642047+ariefrahmanna@users.noreply.github.com>
2025-06-06 19:11:32 +0700

    style: update color pallete

--- lib/app/themes/colors.dart
+++ lib/app/themes/colors.dart
@@ -3,19 +3,19 @@ import 'package:flutter/material.dart';
 const Color primaryColor = Color(0xFF156650);
 
 const Color neutral10 = Color(0xFFFFFFFF);
-const Color neutral20 = Color(0xFFE6E6E6);
+const Color neutral20 = Color(0xFFF5F5F5);
 const Color neutral30 = Color(0xFFCCCCCC);
-const Color neutral40 = Color(0xFFB3B3B3);
-const Color neutral50 = Color(0xFFC2C2C2);
-const Color neutral60 = Color(0xFF999999);
+const Color neutral40 = Color(0xFFEDEDED);
+const Color neutral50 = Color(0xFFE0E0E0);
+const Color neutral60 = Color(0xFF9E9E9E);
 const Color neutral70 = Color(0xFF757575);
-const Color neutral80 = Color(0xFF4D4D4D);
-const Color neutral90 = Color(0xFF1A1A1A);
-const Color neutral100 = Color(0xFF000000);
+const Color neutral80 = Color(0xFF616161);
+const Color neutral90 = Color(0xFF404040);
+const Color neutral100 = Color(0xFF0A0A0A);
 
 const Color text = Color(0xFF404040);
 const Color selectedItemColor = Color(0xFF4CAF50);
 const Color unselectedItemColor = Color(0xFF9E9E9E);
-const Color favorite = Color(0xFFF44336);
+const Color favorite = Color(0xFFE9494F);
 const Color success = Color(0xFF1F5F21);
 const Color errors = Color(0xFF870B02);
