5b21a07475070eeba3ae3b4fbb5091ed5ab295ce
oliverleon <phyromontega@gmail.com>
2025-06-06 10:22:59 +0700

    fix: limit recently added items to the first five in fetchRecentlyAddedPreview

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -22,7 +22,7 @@ class HomeController extends GetxController {
     try {
       isLoading.value = true;
       await recentlyAddedController.fetchRecentlyAddedItems();
-      recentlyAddedItems.value = allItems;
+      recentlyAddedItems.value = allItems.take(5).toList();
     } catch (e) {
       Get.snackbar(
         'Error',
