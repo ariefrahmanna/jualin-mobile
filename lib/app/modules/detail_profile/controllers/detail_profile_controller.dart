57f3fe426241b48dbe19258737577c1a72656a11
Arief Rahman <140642047+ariefrahmanna@users.noreply.github.com>
2025-06-08 22:07:17 +0700

    feat: implement fetch items method

--- lib/app/modules/detail_profile/controllers/detail_profile_controller.dart
+++ lib/app/modules/detail_profile/controllers/detail_profile_controller.dart
@@ -1,12 +1,52 @@
+import 'dart:convert';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'package:jualin/app/themes/colors.dart';
+import 'package:jualin/utils/api_endpoints.dart';
 
 class DetailProfileController extends GetxController {
-  //TODO: Implement DetailProfileController
+  var user = <String, dynamic>{}.obs;
+  var items = <Map<String, dynamic>>[].obs;
+  var isLoading = false.obs;
 
-  final count = 0.obs;
+  Future<void> fetchItems() async {
+    isLoading.value = true;
+    var secureStorage = FlutterSecureStorage();
+
+    try {
+      String? token = await secureStorage.read(key: 'token');
+
+      var response = await http.get(
+        Uri.parse(ApiEndpoints.getItemsByUsername(user['username'])),
+        headers: {'Authorization': 'Bearer $token'},
+      );
+
+      var json = jsonDecode(response.body);
+
+      if (response.statusCode != 200 || !json['status']) {
+        throw json['message'];
+      }
+
+      items.value = json['data'];
+    } catch (e) {
+      Get.snackbar(
+        'Error',
+        e.toString(),
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: AppColors.error,
+        colorText: AppColors.neutral10,
+      );
+    } finally {
+      isLoading.value = false;
+    }
+  }
+
   @override
   void onInit() {
     super.onInit();
+    user.value = Get.arguments['user'];
   }
 
   @override
@@ -18,6 +58,4 @@ class DetailProfileController extends GetxController {
   void onClose() {
     super.onClose();
   }
-
-  void increment() => count.value++;
 }
