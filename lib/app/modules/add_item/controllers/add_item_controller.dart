38c24fc9862f3e4407925c6efe263f462c1babff
Arief Rahman <140642047+ariefrahmanna@users.noreply.github.com>
2025-06-10 20:40:38 +0700

    fix: remove Content-Type header from request

--- lib/app/modules/add_item/controllers/add_item_controller.dart
+++ lib/app/modules/add_item/controllers/add_item_controller.dart
@@ -37,7 +37,6 @@ class AddItemController extends GetxController {
       var url = Uri.parse(ApiEndpoints.items);
       var headers = {
         'Authorization': 'Bearer $token',
-        'Content-Type': 'application/json',
         'Accept': 'application/json',
       };
       var body = {
