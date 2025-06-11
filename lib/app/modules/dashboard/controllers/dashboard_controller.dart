ddabac752a3181e65fe914dd85b2159c5da748f8
oliverleon <phyromontega@gmail.com>
2025-06-11 09:08:05 +0700

    feat: add fetch data whenever a page is selected on dashboard

--- lib/app/modules/dashboard/controllers/dashboard_controller.dart
+++ lib/app/modules/dashboard/controllers/dashboard_controller.dart
@@ -1,12 +1,35 @@
 // ignore_for_file: unnecessary_overrides
 
 import 'package:get/get.dart';
+import 'package:jualin/app/modules/home/controllers/home_controller.dart';
+import 'package:jualin/app/modules/sell_items/controllers/sell_items_controller.dart';
+import 'package:jualin/app/modules/transactions/controllers/transactions_controller.dart';
+import 'package:jualin/app/modules/wishlist/controllers/wishlist_controller.dart';
 
 class DashboardController extends GetxController {
   var selectedIndex = 0.obs;
+  HomeController homeController = Get.put(HomeController());
+  WishlistController wishlistController = Get.put(WishlistController());
+  TransactionsController transactionsController =
+      Get.put(TransactionsController());
+  SellItemsController sellItemsController = Get.put(SellItemsController());
 
   void changePage(int index) {
     selectedIndex.value = index;
+    switch (index) {
+      case 0:
+        homeController.fetchRecentlyAddedPreview();
+        break;
+      case 1:
+        wishlistController.fetchWishlist();
+        break;
+      case 2:
+        transactionsController.fetchTransactions();
+        break;
+      case 3:
+        sellItemsController.fetchSellItems();
+        break;
+    }
   }
 
   void setIndexFromArguments(dynamic args) {
